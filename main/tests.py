daa66499ece039349bef51d08413abd5765a8d96
Muhammad Oka <oka@muhammadoka.dev>
2023-09-11 19:16:03 +0700

    add tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.http import HttpResponse
 
 # Create your tests here.
+class MainTest(TestCase):
+    def test_main_exists(self):
+        response: HttpResponse = Client().get('/')
+        self.assertEquals(response.status_code, 200)
+    
+    def test_main_template_test(self):
+        response: HttpResponse = Client().get('/')
+        self.assertTemplateUsed(response, 'main.html')
+
+    def test_main_information_test(self):
+        response: HttpResponse = Client().get('/')
+        self.assertContains(response, "Muhammad Oka")
+        self.assertContains(response, "PBP KKI")
