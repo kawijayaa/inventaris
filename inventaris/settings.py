7cd5b2e3793103a0384428c41e1606adaaa16219
Muhammad Oka <oka@muhammadoka.dev>
2023-11-21 21:34:53 +0700

    enable cors middleware

--- inventaris/settings.py
+++ inventaris/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'inventaris.urls'
